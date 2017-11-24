829cdcbe25467a6dd9617b86c1679830b369d52d
C. E. Ball <ceball@gmail.com>
2017-11-24 15:44:32 +0000

    Updated own doc to match self.

--- doc/conf.py
+++ doc/conf.py
@@ -1,43 +1,25 @@
 # -*- coding: utf-8 -*-
 
-
-from nbsite.shared_conf import * # noqa
-
-##############################################################
-# start of things to edit
+from nbsite.shared_conf import *
 
 project = u'nbsite'
 authors = u'nbsite GitHub contributors'
 copyright = u'2017 ' + authors
-
-# TODO: rename
-ioam_module = 'nbsite'
 description = 'something'
 
-# TODO: gah, version
-version = '0.0.1'
-release = '0.0.1'
+import nbsite
+version = release = nbsite.__version__
 
 html_static_path = ['_static']
-
 html_theme = 'sphinx_ioam_theme'
-html_theme_options = {
-#    'logo':'images/amazinglogo.png'
-#    'favicon':'images/amazingfavicon.ico'
-# ...
-# ? css
-# ? js
-}
+html_theme_options = {}
 
-
-_NAV =  (
-)
+_NAV = ()
 
 html_context = {
     'PROJECT': project,
     'DESCRIPTION': description,
     'AUTHOR': authors,
-    # will work without this - for canonical (so can ignore when building locally or test deploying)    
     'WEBSITE_SERVER': 'https://ioam.github.io/nbsite',
     'VERSION': version,
     'NAV': _NAV,
@@ -48,8 +30,3 @@ html_context = {
     'js_includes': ['custom.js', 'require.js'],
 }
 
-# end of things to edit
-##############################################################
-
-from nbsite.shared_conf2 import hack
-setup, intersphinx_mapping, texinfo_documents, man_pages, latex_documents, htmlhelp_basename, html_static_path, html_title, exclude_patterns = hack(project,ioam_module,authors,description,html_static_path)
