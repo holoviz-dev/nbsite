196c96e9a99193991a150986d6a017edc94e98f5
Chris B <ceball@gmail.com>
2018-05-25 12:33:01 +0100

    Another go at parsing metadata

--- doc/conf.py
+++ doc/conf.py
@@ -5,10 +5,11 @@ from nbsite.shared_conf import *
 project = 'nbsite'
 
 ####
-# could put in nbsite.util and all projects could use
+# could put in nbsite.util and all projects could use...and...surely there's a simpler way?
 from pkg_resources import get_distribution
+from email import message_from_string
 _pkg = get_distribution(project)
-_m = dict([ tuple(a.split(": ")[0:2]) for a in _pkg._get_metadata(_pkg.PKG_INFO) ])
+_m = {m[0]:m[1] for m in message_from_string(_pkg.get_metadata(_pkg.PKG_INFO)).items()}
 ####
 
 authors = _m["Author"]
