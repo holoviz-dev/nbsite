42bbd2f110a1bad6eefe0ec4e5d7e6615b8fb9f1
Chris B <ceball@gmail.com>
2018-06-14 11:24:16 +0100

    Hacked in examples packaging

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@
 import glob
 from setuptools import setup, find_packages
 
-import version
+# Temporary until build requirements as specified in pyproject.toml
+# are widely supported
+try:
+    import pyctbuild
+except ImportError:
+    raise ImportError("Parambokeh requires pyctbuild to build; please upgrade to pip>=10 and try again (or alternatively, install pyctbuild manually first (e.g. `conda install -c pyviz pyctbuild` or `pip install pyctbuild`)")
+
 
 setup_args = dict(
     name='nbsite',
@@ -68,4 +74,18 @@ setup_args = dict(
 )
 
 if __name__=="__main__":
+
+    # TODO: hope to eliminate the examples handling from here too
+    # (i.e. all lines except setup()), moving it to pyctbuild
+    import os, sys, shutil
+    example_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                'nbsite','examples')
+    if 'develop' not in sys.argv:
+        import pyctbuild.examples
+        pyctbuild.examples.examples(example_path, __file__, force=True)
+    
     setup(**setup_args)
+
+    if os.path.isdir(example_path):
+        shutil.rmtree(example_path)
+
