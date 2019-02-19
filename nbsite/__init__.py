1b4908a1b8928207dc5f653728dc5719fdca782e
Julia Signell <jsignell@gmail.com>
2019-02-19 11:02:01 -0500

    Fixing copy/paste error

--- nbsite/__init__.py
+++ nbsite/__init__.py
@@ -1,6 +1,6 @@
 import param
 
-NAME = "param"
+NAME = "nbsite"
 
 # version comes from git if available, otherwise from .version file
 __version__ = str(param.version.Version(fpath=__file__, archive_commit="$Format:%h$",
