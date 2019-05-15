81de9ca0c76933f079f632fb1d7075b14d32e1ca
Julia Signell <jsignell@gmail.com>
2019-05-15 10:27:11 -0400

    Making build_docs dodo command

--- dodo.py
+++ dodo.py
@@ -3,3 +3,16 @@ if "PYCTDEV_ECOSYSTEM" not in os.environ:
     os.environ["PYCTDEV_ECOSYSTEM"] = "pip"
 
 from pyctdev import *  # noqa: api
+
+
+########## DOCS ##########
+
+def task_build_docs():
+    """build docs"""
+    return {
+        'actions': [
+            'nbsite generate-rst --org pyviz --repo nbsite --skip ".*sites.*"',
+            'nbsite build',
+        ],
+        'verbosity': 2,
+    }
