47bb2aad9f2433ba32d12f58f2b2bc5e5d64b38c
Philipp Rudiger <prudiger@anaconda.com>
2019-06-10 10:20:52 -0500

    Further improvements for Param doc formatting

--- nbsite/paramdoc.py
+++ nbsite/paramdoc.py
@@ -2,13 +2,16 @@ import inspect
 
 import param
 
+from param.parameterized import label_formatter
+
 param.parameterized.docstring_signature = False
 param.parameterized.docstring_describe_params = False
 
 # Parameter attributes which are never shown
 IGNORED_ATTRS = [
     'precedence', 'check_on_set', 'instantiate', 'pickle_default_value',
-    'watchers', 'compute_default_fn']
+    'watchers', 'compute_default_fn', 'doc', 'owner', 'per_instance',
+    'constant', 'is_instance', 'name', 'allow_None']
 
 # Default parameter attribute values (value not shown if it matches defaults)
 DEFAULT_VALUES = {'allow_None': False, 'readonly': False}
@@ -24,13 +27,15 @@ def param_formatter(app, what, name, obj, options, lines):
             if child in ["print_level", "name"]:
                 continue
             pobj = params[child]
+            label = label_formatter(pobj.name)
             doc = pobj.doc or ""
             members = inspect.getmembers(pobj)
             params_str = ""
             for m in members:
                 if (m[0][0] != "_" and m[0] not in IGNORED_ATTRS and
                     not inspect.ismethod(m[1]) and not inspect.isfunction(m[1]) and
-                    m[1] is not None and DEFAULT_VALUES.get(m[0]) != m[1]):
+                    m[1] is not None and DEFAULT_VALUES.get(m[0]) != m[1] and
+                    (m[0] != 'label' or pobj.label != label)):
                     params_str += "%s=%s, " % (m[0], repr(m[1]))
             params_str = params_str[:-2]
             ptype = params[child].__class__.__name__
