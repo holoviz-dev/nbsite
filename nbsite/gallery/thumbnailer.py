06af9d59e5345ba1ea558b41608af79b83fdfd3e
jlstevens <jlrstevens@gmail.com>
2020-02-12 13:25:06 -0600

    Inlined comment_out_magics utility as well

--- nbsite/gallery/thumbnailer.py
+++ nbsite/gallery/thumbnailer.py
@@ -10,6 +10,18 @@ from holoviews.util.command import export_to_python
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
+def comment_out_magics(source):
+    """
+    Utility used to make sure AST parser does not choke on unrecognized
+    magics.
+    """
+    filtered = []
+    for line in source.splitlines():
+        if line.strip().startswith('%'):
+            filtered.append('# ' +  line)
+        else:
+            filtered.append(line)
+    return '\n'.join(filtered)
 
 def wrap_cell_expression(source, template='{expr}'):
     """
