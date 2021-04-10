910e81ea236c6e12b08c632988fbefecf6cee076
Philipp Rudiger <prudiger@anaconda.com>
2021-04-10 18:18:29 +0200

    Revert test changes

--- dodo.py
+++ dodo.py
@@ -1,6 +1,6 @@
 import os
 if "PYCTDEV_ECOSYSTEM" not in os.environ:
-    os.environ["PYCTDEV_ECOSYSTEM"] = "conda"
+    os.environ["PYCTDEV_ECOSYSTEM"] = "pip"
 
 from pyctdev import *  # noqa: api
 
