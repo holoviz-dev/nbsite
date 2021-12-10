33606bfa565e35383b62fb61671d912a95b1f8fb
Maxime Liquet <35924738+maximlt@users.noreply.github.com>
2021-12-10 02:33:35 -0800

    suppport post versions of a final release (#205)

--- nbsite/tests/test_util.py
+++ nbsite/tests/test_util.py
@@ -11,6 +11,8 @@ from nbsite.util import base_version
         ("0.13.0b1", "0.13.0b1"),
         ("no.match", "no.match"),
         ("0.13.0a19.post4+g0695e214", "0.13.0a19"),
+        ("1.14.4", "1.14.4"),
+        ("1.14.4.post82+g46ba8bbf2", "1.14.4"),
         # Only 3 compontents version are matched.
         ("0.13.post4+g0695e214", "0.13.post4+g0695e214"),
         ("v0.3.0", "v0.3.0"),
