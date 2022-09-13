46a147a10dda313035f926eea392f95e034fee9e
Philipp Rudiger <prudiger@anaconda.com>
2022-09-13 15:41:52 +0200

    Ensure warning is shown

--- nbsite/pyodide/_static/run_cell.js
+++ nbsite/pyodide/_static/run_cell.js
@@ -2,25 +2,24 @@
  * SVG files for our copy buttons
  */
 let iconRun = `
-<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+<svg class="pyodide-run-icon" style="width:24px;height:24px" viewBox="0 0 24 24">
     <path stroke="none" fill="#28a745" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
 </svg>`
 
 let iconLoading = `
-<svg class="loading" style="width:24px;height:24px" viewBox="0 0 24 24">
+<svg class="pyodide-loading-icon" class="loading" style="width:24px;height:24px" viewBox="0 0 24 24">
     <path fill="currentColor" d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" />
 </svg>`
 
-let iconLoaded = `
-<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+let iconLoaded = `<svg class="pyodide-loaded-icon" style="width:24px;height:24px" viewBox="0 0 24 24">
     <path fill="#28a745" d="M9,20.42L2.79,14.21L5.62,11.38L9,14.77L18.88,4.88L21.71,7.71L9,20.42Z" />
 </svg>`
 
-let iconError = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+let iconError = `<svg class="pyodide-error-icon" style="width:24px;height:24px" viewBox="0 0 24 24">
     <path fill="#ff0000" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
 </svg>`
 
-let iconAlert = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+let iconAlert = `<svg class="pyodide-alert-icon" style="width:24px;height:24px" viewBox="0 0 24 24">
     <path fill="#f6be00" d="M13 13H11V7H13M11 15H13V17H11M15.73 3H8.27L3 8.27V15.73L8.27 21H15.73L21 15.73V8.27L15.73 3Z" />
 </svg>`
 
@@ -100,7 +99,7 @@ const _addRunButtonToCodeCells = () => {
     const run_button = document.getElementById(`button-${id}`)
     run_button.addEventListener('click', (e) => {
       if (!ACCEPTED) {
-	_ChangeTooltip(e.currentTarget, 'Executing this cell will download Python runtime (~80+ MB). Click again to proceed.')
+	_ChangeTooltip(e.currentTarget, 'Executing this cell will download Python runtime (typically 80+ MB). Click again to proceed.')
 	_ChangeIcon(e.currentTarget, iconAlert)
 	ACCEPTED = true
 	return
