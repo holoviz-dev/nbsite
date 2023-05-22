580fd838c315f3b18fd7567c2feafc693c003184
Philipp Rudiger <prudiger@anaconda.com>
2023-05-22 14:29:17 +0200

    Run pre-commit in CI

--- setup.py
+++ setup.py
@@ -24,13 +24,16 @@ def get_setup_version(root, reponame):
     version_file_path = os.path.join(filepath, reponame, '.version')
     try:
         from param import version
-    except:
+    except Exception:
         version = None
     if version is not None:
         return version.Version.setup_version(filepath, reponame, archive_commit="$Format:%h$")
-    else:
-        print("WARNING: param>=1.6.0 unavailable. If you are installing a package, this warning can safely be ignored. If you are creating a package or otherwise operating in a git repository, you should install param>=1.6.0.")
-        return json.load(open(version_file_path, 'r'))['version_string']
+    print(
+        "WARNING: param>=1.6.0 unavailable. If you are installing a package, "
+        "this warning can safely be ignored. If you are creating a package or "
+        "otherwise operating in a git repository, you should install param>=1.6.0."
+    )
+    return json.load(open(version_file_path, 'r'))['version_string']
 
 setup_args = dict(
     name=NAME,
