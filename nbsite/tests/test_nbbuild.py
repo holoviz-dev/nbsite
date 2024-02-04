4364ce061406b2543150ed8683d1b770a0ec8996
Niko Föhr <fohrloop@gmail.com>
2024-02-04 15:16:49 +0200

    refactor test: use pytest.mark.parametrize

--- nbsite/tests/test_nbbuild.py
+++ nbsite/tests/test_nbbuild.py
@@ -1,9 +1,16 @@
+import pytest
+
 from nbsite.nbbuild import FixNotebookLinks
 
 
 class TestFixNotebookLinks:
 
-
-    def test_get_links(self):
-        assert list(FixNotebookLinks._get_links('foo [a](b.ipynb) bar')) == [('[a](b.ipynb)', 'b.ipynb')]
-        assert list(FixNotebookLinks._get_links('foo [a](b.md) bar')) == []
+    @pytest.mark.parametrize(
+        "markdowntext, expected_output",
+        [
+            ("foo [a](b.ipynb) bar", [("[a](b.ipynb)", "b.ipynb")]),
+            ("foo [a](b.md) bar", []),
+        ],
+    )
+    def test_get_links(self, markdowntext, expected_output):
+        assert list(FixNotebookLinks._get_links(markdowntext)) == expected_output
