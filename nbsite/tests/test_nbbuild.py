d72bfdc49ffecfc1aaac4b40afa7a586beb35a62
Niko Föhr <fohrloop@gmail.com>
2024-02-04 15:21:18 +0200

    test for two links in text

--- nbsite/tests/test_nbbuild.py
+++ nbsite/tests/test_nbbuild.py
@@ -8,8 +8,15 @@ class TestFixNotebookLinks:
     @pytest.mark.parametrize(
         "markdowntext, expected_output",
         [
+            # Case: one link in the text
             ("foo [a](b.ipynb) bar", [("[a](b.ipynb)", "b.ipynb")]),
+            # Case: no link to .ipynb
             ("foo [a](b.md) bar", []),
+            # Case: two links in the text
+            (
+                "foo [a](b.ipynb) bar [c](d.ipynb) baz.",
+                [("[a](b.ipynb)", "b.ipynb"), ("[c](d.ipynb)", "d.ipynb")],
+            ),
         ],
     )
     def test_get_links(self, markdowntext, expected_output):
