3269381fb2daf510430c67ef2aba62c80a685849
maximlt <mliquet@anaconda.com>
2024-10-16 10:54:54 +0200

    simplify implementation

--- nbsite/nb_interactivity_warning/__init__.py
+++ nbsite/nb_interactivity_warning/__init__.py
@@ -4,7 +4,6 @@ a Jupyter or MyST Markdown notebook, similarly to the warning added by
 the NotebookDirective.
 
 This extension depends on:
-- nbsite/_shared_templates/nb-interactivity-warning.html
 - nbsite/_shared_static/scroller.css (also needed for the NotebookDirective)
 """
 
@@ -16,38 +15,32 @@ from .. import __version__ as nbs_version
 logger = getLogger(__name__)
 
 
-def add_sidebar_content(
+HTML_INTERACTIVITY_WARNING = """
+<div id="scroller-right">
+    This web page was generated from a Jupyter notebook and not all
+    interactivity will work on this website.
+</div>
+"""
+
+
+def add_interactivity_warning_to_body(
     app: Sphinx, pagename: str, templatename: str, context: dict, doctree
 ):
     """
-    Adds custom content to the right sidebar if the page is a Jupyter Notebook.
+    Adds custom content to context body if the page is a Jupyter Notebook.
     """
     if context.get("page_source_suffix") == ".ipynb" or (
         context.get("page_source_suffix") == ".md"
         # There might be other ways to find this out, seems to work.
         and "kernelspec" in app.env.metadata[pagename]
     ):
-        sidebar_items = context.get("theme_secondary_sidebar_items", [])
-        if "nb-interactivity-warning" in sidebar_items:
-            return
-        # theme_secondary_sidebar_items can be a list or a comma-separated string.
-        if isinstance(sidebar_items, str):
-            if sidebar_items:
-                sidebar_items = ", ".join([sidebar_items, "nb-interactivity-warning"])
-            else:
-                sidebar_items = "nb-interactibity-warning"
-        else:
-            sidebar_items.append("nb-interactivity-warning")
-        context["theme_secondary_sidebar_items"] = sidebar_items
-        logger.debug(
-            "Adding 'nb-interactivity-warning' item to the secondary sidebar: %s",
-            pagename,
-        )
+        if HTML_INTERACTIVITY_WARNING not in context.get('body', ''):
+            context['body'] += HTML_INTERACTIVITY_WARNING
 
 
 def setup(app: Sphinx):
     # Event triggered when HTML pages are built
-    app.connect("html-page-context", add_sidebar_content)
+    app.connect("html-page-context", add_interactivity_warning_to_body)
     return {
         "version": nbs_version,
         "parallel_read_safe": True,
