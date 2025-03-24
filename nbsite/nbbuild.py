8a94d881cf0451711fbdaef75ba56f139348b9d3
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-03-24 12:59:57 +0100

    fix logger.warn warning

--- nbsite/nbbuild.py
+++ nbsite/nbbuild.py
@@ -230,7 +230,7 @@ class FixNotebookLinks(Preprocessor):
 
             target_relpath = cls._get_sourcefile(rootdir, nb_filepath)
             if not target_relpath:
-                logger.warn('Source file for "%s" not found (path relative to "%s")', nb_filepath, rootdir)
+                logger.warning('Source file for "%s" not found (path relative to "%s")', nb_filepath, rootdir)
                 continue
 
             new_link = cls._create_target_link(nb_link, target_relpath)
