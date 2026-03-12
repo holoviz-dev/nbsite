3927f3e1006de9dc9df9ff7dca1c5ee347af0000
Simon Høxbro Hansen <hoxbro@protonmail.com>
2026-03-12 09:14:39 +0100

    fix: Add HoloViews magic

--- nbsite/gallery/thumbnailer.py
+++ nbsite/gallery/thumbnailer.py
@@ -160,12 +160,124 @@ def execute(code, cwd, env):
         proc.kill()
     return proc.returncode
 
-def notebook_thumbnail(filename, subpath):
-    from holoviews.ipython.preprocessors import (
-        OptsMagicProcessor, OutputMagicProcessor, StripMagicsProcessor,
-    )
-    from holoviews.util.command import export_to_python
 
+def export_to_python(filename, preprocessors):
+    import nbconvert
+    import nbformat
+
+    with open(filename) as f:
+        nb = nbformat.read(f, nbformat.NO_CONVERT)
+        exporter = nbconvert.PythonExporter()
+        for preprocessor in preprocessors:
+            exporter.register_preprocessor(preprocessor)
+        source, _meta = exporter.from_notebook_node(nb)
+        return source
+
+
+def filter_magic(source, magic, strip=True):
+    """Given the source of a cell, filter out the given magic and collect
+    the lines using the magic into a list.
+
+    If strip is True, the IPython syntax part of the magic (e.g. %magic
+    or %%magic) is stripped from the returned lines.
+
+    """
+    filtered, magic_lines=[],[]
+    for line in source.splitlines():
+        if line.strip().startswith(magic):
+            magic_lines.append(line)
+        else:
+            filtered.append(line)
+    if strip:
+        magic_lines = [el.replace(magic,'') for el in magic_lines]
+    return '\n'.join(filtered), magic_lines
+
+def strip_magics(source):
+    """Given the source of a cell, filter out all cell and line magics.
+
+    """
+    filtered=[]
+    for line in source.splitlines():
+        if not line.startswith('%'):
+            filtered.append(line)
+    return '\n'.join(filtered)
+
+
+def replace_line_magic(source, magic, template='{line}'):
+    """Given a cell's source, replace line magics using a formatting
+    template, where {line} is the string that follows the magic.
+
+    """
+    filtered = []
+    for line in source.splitlines():
+        if line.strip().startswith(magic):
+            substitution = template.format(line=line.replace(magic, ''))
+            filtered.append(substitution)
+        else:
+            filtered.append(line)
+    return '\n'.join(filtered)
+
+
+
+class OptsMagicProcessor(Preprocessor):
+    """Preprocessor to convert notebooks to Python source to convert use of
+    opts magic to use the util.opts utility instead.
+
+    """
+
+    def preprocess_cell(self, cell, resources, index):
+        if cell['cell_type'] == 'code':
+            source = replace_line_magic(cell['source'], '%opts',
+                                        template='hv.util.opts({line!r})')
+            source, opts_lines = filter_magic(source, '%%opts')
+            if opts_lines:
+                # Escape braces (e.g. normalization options) as they pass through format
+                template = 'hv.util.opts({options!r}, {{expr}})'.format(
+                    options=' '.join(opts_lines).replace('{','{{').replace('}','}}'))
+                source = wrap_cell_expression(source, template)
+            cell['source'] = source
+        return cell, resources
+
+    def __call__(self, nb, resources): return self.preprocess(nb,resources)
+
+
+class OutputMagicProcessor(Preprocessor):
+    """Preprocessor to convert notebooks to Python source to convert use of
+    output magic to use the util.output utility instead.
+
+    """
+
+    def preprocess_cell(self, cell, resources, index):
+        if cell['cell_type'] == 'code':
+            source = replace_line_magic(cell['source'], '%output',
+                                        template='hv.util.output({line!r})')
+            source, output_lines = filter_magic(source, '%%output')
+            if output_lines:
+                template = f'hv.util.output({output_lines[-1]!r}, {{expr}})'
+                source = wrap_cell_expression(source, template)
+
+            cell['source'] = source
+        return cell, resources
+
+    def __call__(self, nb, resources): return self.preprocess(nb,resources)
+
+
+class StripMagicsProcessor(Preprocessor):
+    """Preprocessor to convert notebooks to Python source to strips out all
+    magics. To be applied after the preprocessors that can handle
+    holoviews magics appropriately.
+
+    """
+
+    def preprocess_cell(self, cell, resources, index):
+        if cell['cell_type'] == 'code':
+            cell['source'] = strip_magics(cell['source'])
+        return cell, resources
+
+    def __call__(self, nb, resources): return self.preprocess(nb,resources)
+
+
+def notebook_thumbnail(filename, subpath):
     basename = os.path.splitext(os.path.basename(filename))[0]
     dir_path = os.path.abspath(os.path.join(subpath, 'thumbnails'))
     absdirpath= os.path.abspath(os.path.join('.', dir_path))
