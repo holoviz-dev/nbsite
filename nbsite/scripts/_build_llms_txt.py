ac8dec0b29491d0b39958de77f6276b5a9019f0e
Isaiah Akorita <akoritaisaiah@gmail.com>
2026-08-24 15:38:35 +0100

    add comment

--- nbsite/scripts/_build_llms_txt.py
+++ nbsite/scripts/_build_llms_txt.py
@@ -81,7 +81,7 @@ _META_REFRESH_RE = re.compile(
     re.I | re.S,
 )
 
-
+# Should match its counterpart file name in the html docs, i.e Introduction.html -> Introduction.md.
 _NUM_PREFIX_RE = re.compile(r"^\d+[-_ ]")
 
 
